277bc834fcde404d108c5d8792a6f1318ce0bd5f
224jfarrell <224jfarrell@frhsd.com>
2024-03-13 11:02:32 -0400

    broooooo

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
-let A = document.getElementById('container');
-let num;
+let A = document.getElementsByClassName('letter');
+let B = document.getElementById('filler');
+let C = document.getElementById('fullpage');
+let D = getComputedStyle(C);
+let E = (window.outerHeight - (parseFloat(D.height) + "px"))
+console.log(parseFloat(D.height))
+console.log(E)
+
+
 function it(){
-    setInterval(num, function(){
+    random();
+}
+
+function random(){
+    let num = Math.random() * 6666;
+    italicInt = setInterval(num, function(){
         console.log(num);
         A.style.fontStyle = "italic";
-        setTimeout(100, function(){
+        setTimeout(1000, function(){
             A.style.fontStyle = "none";
         });
     });
+    console.log(italicInt);
 }
